b1428651b67992891907ef22bb3123618a577014
MuthammalMahendiran <muthammalm911@pec.edu>
2022-04-03 03:56:28 -0700

    commit

--- documentation/inc/Natural_Gardening_System.h
+++ documentation/inc/Natural_Gardening_System.h
@@ -8,3 +8,62 @@
  * @copyright Copyright (c) 2022
  * 
  */
+#ifndef __NATURAL_GARDENING_SYSTEM_H_
+#define __NATURAL_GARDENING_SYSTEM_H_
+
+#include "stdio.h"
+#include "string.h"
+#include "conio.h"
+
+/**
+* @brief order,serve,display and exit the  plants,seeds and fertilizers
+* @param[in] order
+* @param[in] serve
+*@param[in] display 
+*@param[in] exit
+* @return Result of order,serve,display and exit the plants, seeds and fertilizers
+*/
+int order()
+
+/**
+* @brief order
+* @param[in] Enter chosen item code
+* @param[in] Enter the Herbs: 1.Basil  2.Dill 3.Mint 4.Rosemary 5.Cilantro
+ *@param[in] Enter the Shrubs: 1.Croton  2.Lemon 3.Tulsi 4.Heena mehndi 5.China rose
+ *@param[in] Enter the Trees: 1. Mango  2.Neem  3.Banyan 4.Palm 5.Coconut
+ *@param[in] Enter the Climbers: 1.Sweet gourd 2.Aparajita 3.Flame Plant 4.Money plant 5.Star Jasmine
+ *@param[in] Enter the Creepers: 1.Watermelon 2.Pumpkin 3.Cucumber 4.Musk melon 5.Bottle ground
+ *@param[in] Enter the Water plants: 1.Water Hyacinth 2.Lotus 3.Trapa 4.Water Lilly 5.Hydrilla
+ *@param[in] Enter the Wild plants: 1.Datura 2.Nagfani 3.Madar 4.Satyanashi 5.Marua
+ *@param[in] Enter  the Seeds: 1.Sunflower  2.Rice 3.Peas 4.Bean 5.Corn
+ *@param[in] Enter the Organic Fertilizers: 1.Kelp 2.Cow dung manure 3.Alfalfa meal 4.Limestone 5.Chicken manure
+ *@param[in] Enter the Inorganic Fertilizers: 1.Ammonium nitrate 2.Potassium sulfate 3.Superphosphate 4.Lime 5.Rock
+ * @return Result
+*/
+int recieve()
+
+/**
+* @brief serve the order
+* @param[in] Please order first
+* @param[in] All order served 
+* @param[in] Your order is ready
+* @return Result of serve the order
+*/
+int display()
+
+/**
+* @brief Display the order
+* @param[in] Total order taken
+* @param[in] Total number of order served
+* @param[in] Number of Currently waiting to be served
+* @param[in] Current  orders
+* @return Result of display
+*/
+int exit()
+/**
+* @brief exit
+* @param[in] Thank you! Save Trees Save life
+* @return Result of exit
+*/
+
+#endif  /* #define _NATURAL_GARDENING_SYSTEM_H_*/
